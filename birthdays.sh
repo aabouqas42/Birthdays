59550772e0865bdfc4fb072d794266850ae4f1b7
AissamDEV <aissamaboukassim@gmail.com>
2023-06-28 05:54:28 -0700

    update

--- birthdays.sh
+++ birthdays.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
-<<<<<<< HEAD
 echo " "
 echo -e "		\e[32m<<< BIRTHDAYS >>>\e[0m\n"
-echo -e ">>> Enter the day <<<\n"
-read -p '>> ' dd
-echo -e "\n>>> Enter the month <<<\n"
-read -p '>> ' mm
-echo -e "\n>>> Enter the year <<<\n"
-=======
-echo -e "\n\e[34m<<< BIRTHDAYS >>>\e[0m\n"
-echo -- Enter the day --
-read -p '>> ' dd
-echo -- Enter the month --
-read -p '>> ' mm
-echo -- Enter the year --
->>>>>>> e1210498b7f528a49e9bccc7c218b2938a46dea1
-read -p '>> ' yy
+echo -e "  >>> Enter the day <<<\n"
+read -p '  >> ' dd
+echo -e "\n  >>> Enter the month <<<\n"
+read -p '  >> ' mm
+echo -e "\n  >>> Enter the year <<<\n"
+read -p '  >> ' yy
 if [[ $mm =~ ^[0-9]{1}$ ]]
 then
 	mm="0$mm"
 fi
-echo -e "\n\e[32m<<< You was born in [$dd/$mm/$yy] >>>\e[0m\n"
+echo -e "\n        \e[32m<<< You was born in [$dd/$mm/$yy] >>>\e[0m\n"
 birthday="$yy-$mm-$dd"
 curr=$(date +%s%3N)
 milliseconds=$(date -d "$birthday" +%s%3N)
@@ -30,12 +21,6 @@ years=$(echo "$days / 365.25" | bc)
 months=$(echo "(($days - ($years * 365.25)) / 30.44)" | bc)
 days_remaining=$(echo "(($days - ($years * 365.25)) % 30.44)" | bc)
 int=$(echo "$days_remaining" | awk '{printf("%d\n",$0)}')
-<<<<<<< HEAD
-#echo -e "\e[35m<<< Your age is $years year(s) & $months month(s) & $int day(s) >>>\e[0m\n"
-=======
-int=$(echo "($int -2)" | bc)
-echo -e ">>> Your age is $years year(s) & $months month(s) & $int day(s) <<<\n"
->>>>>>> e1210498b7f528a49e9bccc7c218b2938a46dea1
 current_year=$(date +%Y)
 next_berthday=$(date -d "$mm/$dd/$current_year" +%s)
 if [[ $next_birthday -lt $(date +%s) ]]; then
@@ -48,17 +33,13 @@ next_days_remaining=$(echo "(($next_days - ($next_years * 365.25)) % 30.44)" | b
 next_months=$(echo "(($next_days - ($next_years * 365.25)) / 30.44)" | bc)
 next_days_remaining_int=$(echo "$next_days_remaining" | awk '{printf("%d\n",$0)}')
 weeks=$(echo "($next_months / 7)" | bc)
-<<<<<<< HEAD
 current_date=$(date +%d%m%Y)
 birth_date=$dd$mm$current_year
 if [ $current_date -eq $birth_date ]
 then
-	echo -e "\e[42m<<< Your age is $years years >>>\e[0m\n"
-	echo -e "	\e[32mHappy , Birthday..!\e[0m\n"
+	echo -e "	     \e[42m<<< Your age is $years years >>>\e[0m\n"
+	echo -e "	           \e[32mHappy , Birthday..!\e[0m\n"
 else
 	echo -e "\e[36m<<< Your age is $years year(s) & $months month(s) & $int day(s) >>>\e[0m\n"
-	echo -e "\e[32mYour next birthday after $next_months month(s) & $weeks week(s) & $next_days_remaining_int day(s)\e[0m\n"
+	echo -e "\e[32m<<< Your next birthday after $next_months month(s) & $weeks week(s) & $next_days_remaining_int day(s)\e[0m >>>\n"
 fi
-=======
-echo -e ">>> Your next birthday after $next_months month(s) & $weeks week(s) & $next_days_remaining_int day(s) <<<<\n"
->>>>>>> e1210498b7f528a49e9bccc7c218b2938a46dea1
